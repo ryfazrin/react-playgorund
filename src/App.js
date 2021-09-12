622d6ac99b4165a5a941b2c56e240bc1d005a7d6
ryfazrin <riansera515@gmail.com>
2021-09-12 16:07:48 +0800

    Looping Lists

--- src/App.js
+++ src/App.js
@@ -8,10 +8,23 @@ function App() {
     setTitle("Title Changed");
   }
 
+  const[products, setProducts] = useState ([
+    {id: 1, title: 'Product 1', price: 899},
+    {id: 2, title: 'Product 2', price: 982},
+    {id: 3, title: 'Product 3', price: 322},
+    {id: 4, title: 'Product 4', price: 763},
+    {id: 5, title: 'Product 5', price: 389}
+  ]);
+
   return (
     <div>
       <h2>{ title }</h2>
       <button onClick={ changeTitle }>Change Title</button>
+      <ul>
+        {products.map((product) => (
+          <li key={ product.id }>{ product.title } - { product.price }</li>
+        ))}
+      </ul>
     </div>
   );
 }
