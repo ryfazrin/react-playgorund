bd22aebfb77ece2a90964b3b40576030a2fca76b
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-19 12:43:03 +0800

    CRUD create function
    add Create function

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import About from './components/About';
-import Contact from './components/Contact';
-import ProductList from './ProductList';
+import AddProduct from './components/AddProduct';
+import ProductList from './components/ProductList';
 
 function App() {
-  const [products, setProducts] = useState ([
-    {id: 1, title: 'Product 1', price: 899},
-    {id: 2, title: 'Product 2', price: 982},
-    {id: 3, title: 'Product 3', price: 322},
-    {id: 4, title: 'Product 4', price: 763},
-    {id: 5, title: 'Product 5', price: 389}
-  ]);
-
-  const deleteProduct = (productId) => {
-    const newProducts = products.filter(product => product.id !== productId);
-    setProducts(newProducts);
-  };
-
   return (
     <div>
       <Router>
         <Switch>
           <Route exact path="/">
-            <ProductList products={ products }  deleteProduct={ deleteProduct } />
+            <ProductList />
           </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
+          <Route exact path="/add">
+            <AddProduct />
           </Route>
         </Switch>
       </Router>
