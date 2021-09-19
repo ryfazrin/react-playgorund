c32bb7bb9252a5b4108a52f561f450e69178dd77
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-19 09:07:32 +0800

    add useEffect with props

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ function App() {
     setTitle("Title Changed");
   }
 
-  const[products, setProducts] = useState ([
+  const [products, setProducts] = useState ([
     {id: 1, title: 'Product 1', price: 899},
     {id: 2, title: 'Product 2', price: 982},
     {id: 3, title: 'Product 3', price: 322},
     {id: 4, title: 'Product 4', price: 763},
     {id: 5, title: 'Product 5', price: 389}
   ]);
+
+  const [name, setName] = useState('Ryan');
 
   const deleteProduct = (productId) => {
     const newProducts = products.filter(product => product.id !== productId);
@@ -24,13 +26,15 @@ function App() {
 
   useEffect(() => {
     console.log('Use Effect Running');
-  }, []);
+  }, [name]);
 
   return (
     <div>
       <h2>{ title }</h2>
       <button onClick={ changeTitle }>Change Title</button>
       <ProductList products={ products }  deleteProduct={ deleteProduct } />
+      <button onClick={ () => setName('John') }>Change Name</button>
+      <p>Name : { name }</p>
     </div>
   );
 }
